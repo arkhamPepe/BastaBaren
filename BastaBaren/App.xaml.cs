ce9b17463c08e76bcd46f6adf96d843fcaa4b5df
Alexander Simola Bergsten <49064742+alexandersimolaIT@users.noreply.github.com>
2022-08-03 23:05:17 +0200

    Add project files.

---
+++ BastaBaren/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+
+namespace BastaBaren;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+#if ANDROID
+		
+#endif
+
+		MainPage = new AppShell();
+	}
+}
